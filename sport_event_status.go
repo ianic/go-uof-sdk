e208f17b57af0d89694eeee15f695006dbf09f01
Igor Anic <ianic@inet.hr>
2020-08-13 12:04:47 +0200

    comment SportEventStatus
    Which attribute belogs to which sport.

--- sport_event_status.go
+++ sport_event_status.go
@@ -20,6 +20,8 @@ type SportEventStatus struct {
 	AwayScore *int `xml:"-" json:"awayScore,omitempty" bson:"awayScore,omitempty"`
 	// Sports-specific integer code the represents the live match status (first period, 2nd break, etc.).
 	MatchStatus *int `xml:"match_status,attr" json:"matchStatus" bson:"matchStatus,omitempty"`
+
+	// Tennis
 	// The player who has the serve at that moment.
 	CurrentServer *Team `xml:"current_server,attr,omitempty" json:"currentServer,omitempty" bson:"currentServer,omitempty"`
 	// The point score of the "home" player. The score will be 50 if the "home"
@@ -30,40 +32,60 @@ type SportEventStatus struct {
 	// The point score of the "away" player. The score will be 50 if the "away"
 	// player has advantage. This attribute is also used for the tiebreak score
 	// when the game is in a tiebreak.
-	AwayGamescore *int `xml:"away_gamescore,attr,omitempty" json:"awayGamescore,omitempty" bson:"awayGamescore,omitempty"`
+	AwayGamescore *int  `xml:"away_gamescore,attr,omitempty" json:"awayGamescore,omitempty" bson:"awayGamescore,omitempty"`
+	Tiebreak      *bool `xml:"tiebreak,attr,omitempty" json:"tiebreak,omitempty" bson:"tiebreak,omitempty"`
+	// Table tennis
+	ExpediteMode *bool `xml:"expedite_mode,attr,omitempty" json:"expediteMode,omitempty" bson:"expediteMode,omitempty"`
 
-	HomePenaltyScore   *int    `xml:"home_penalty_score,attr,omitempty" json:"homePenaltyScore,omitempty" bson:"homePenaltyScore,omitempty"`
-	AwayPenaltyScore   *int    `xml:"away_penalty_score,attr,omitempty" json:"awayPenaltyScore,omitempty" bson:"awayPenaltyScore,omitempty"`
-	HomeLegscore       *int    `xml:"home_legscore,attr,omitempty" json:"homeLegscore,omitempty" bson:"homeLegscore,omitempty"`
-	AwayLegscore       *int    `xml:"away_legscore,attr,omitempty" json:"awayLegscore,omitempty" bson:"awayLegscore,omitempty"`
-	ExpediteMode       *bool   `xml:"expedite_mode,attr,omitempty" json:"expediteMode,omitempty" bson:"expediteMode,omitempty"`
-	Tiebreak           *bool   `xml:"tiebreak,attr,omitempty" json:"tiebreak,omitempty" bson:"tiebreak,omitempty"`
-	HomeSuspend        *int    `xml:"home_suspend,attr,omitempty" json:"homeSuspend,omitempty" bson:"homeSuspend,omitempty"`
-	AwaySuspend        *int    `xml:"away_suspend,attr,omitempty" json:"awaySuspend,omitempty" bson:"awaySuspend,omitempty"`
-	Balls              *int    `xml:"balls,attr,omitempty" json:"balls,omitempty" bson:"balls,omitempty"`
-	Strikes            *int    `xml:"strikes,attr,omitempty" json:"strikes,omitempty" bson:"strikes,omitempty"`
-	Outs               *int    `xml:"outs,attr,omitempty" json:"outs,omitempty" bson:"outs,omitempty"`
-	Bases              *string `xml:"bases,attr,omitempty" json:"bases,omitempty" bson:"bases,omitempty"`
-	HomeBatter         *int    `xml:"home_batter,attr,omitempty" json:"homeBatter,omitempty" bson:"homeBatter,omitempty"`
-	AwayBatter         *int    `xml:"away_batter,attr,omitempty" json:"awayBatter,omitempty" bson:"awayBatter,omitempty"`
-	Possession         *int    `xml:"possession,attr,omitempty" json:"possession,omitempty" bson:"possession,omitempty"`
-	Position           *int    `xml:"position,attr,omitempty" json:"position,omitempty" bson:"position,omitempty"`
-	Try                *int    `xml:"try,attr,omitempty" json:"try,omitempty" bson:"try,omitempty"`
-	Yards              *int    `xml:"yards,attr,omitempty" json:"yards,omitempty" bson:"yards,omitempty"`
-	Throw              *int    `xml:"throw,attr,omitempty" json:"throw,omitempty" bson:"throw,omitempty"`
-	Visit              *int    `xml:"visit,attr,omitempty" json:"visit,omitempty" bson:"visit,omitempty"`
-	RemainingReds      *int    `xml:"remaining_reds,attr,omitempty" json:"remainingReds,omitempty" bson:"remainingReds,omitempty"`
-	Delivery           *int    `xml:"delivery,attr,omitempty" json:"delivery,omitempty" bson:"delivery,omitempty"`
-	HomeRemainingBowls *int    `xml:"home_remaining_bowls,attr,omitempty" json:"homeRemainingBowls,omitempty" bson:"homeRemainingBowls,omitempty"`
-	AwayRemainingBowls *int    `xml:"away_remaining_bowls,attr,omitempty" json:"awayRemainingBowls,omitempty" bson:"awayRemainingBowls,omitempty"`
-	CurrentEnd         *int    `xml:"current_end,attr,omitempty" json:"currentEnd,omitempty" bson:"currentEnd,omitempty"`
-	Innings            *int    `xml:"innings,attr,omitempty" json:"innings,omitempty" bson:"innings,omitempty"`
-	Over               *int    `xml:"over,attr,omitempty" json:"over,omitempty" bson:"over,omitempty"`
-	HomePenaltyRuns    *int    `xml:"home_penalty_runs,attr,omitempty" json:"homePenaltyRuns,omitempty" bson:"homePenaltyRuns,omitempty"`
-	AwayPenaltyRuns    *int    `xml:"away_penalty_runs,attr,omitempty" json:"awayPenaltyRuns,omitempty" bson:"awayPenaltyRuns,omitempty"`
-	HomeDismissals     *int    `xml:"home_dismissals,attr,omitempty" json:"homeDismissals,omitempty" bson:"homeDismissals,omitempty"`
-	AwayDismissals     *int    `xml:"away_dismissals,attr,omitempty" json:"awayDismissals,omitempty" bson:"awayDismissals,omitempty"`
-	CurrentCtTeam      *Team   `xml:"current_ct_team,attr,omitempty" json:"currentCtTeam,omitempty" bson:"currentCtTeam,omitempty"`
+	//Ice hockey
+	HomePenaltyScore *int `xml:"home_penalty_score,attr,omitempty" json:"homePenaltyScore,omitempty" bson:"homePenaltyScore,omitempty"`
+	AwayPenaltyScore *int `xml:"away_penalty_score,attr,omitempty" json:"awayPenaltyScore,omitempty" bson:"awayPenaltyScore,omitempty"`
+
+	// Ice Hockey, Handball, Futsal
+	HomeSuspend *int `xml:"home_suspend,attr,omitempty" json:"homeSuspend,omitempty" bson:"homeSuspend,omitempty"`
+	AwaySuspend *int `xml:"away_suspend,attr,omitempty" json:"awaySuspend,omitempty" bson:"awaySuspend,omitempty"`
+
+	// Baseball
+	Balls      *int    `xml:"balls,attr,omitempty" json:"balls,omitempty" bson:"balls,omitempty"`
+	Strikes    *int    `xml:"strikes,attr,omitempty" json:"strikes,omitempty" bson:"strikes,omitempty"`
+	Outs       *int    `xml:"outs,attr,omitempty" json:"outs,omitempty" bson:"outs,omitempty"`
+	Bases      *string `xml:"bases,attr,omitempty" json:"bases,omitempty" bson:"bases,omitempty"`
+	HomeBatter *int    `xml:"home_batter,attr,omitempty" json:"homeBatter,omitempty" bson:"homeBatter,omitempty"`
+	AwayBatter *int    `xml:"away_batter,attr,omitempty" json:"awayBatter,omitempty" bson:"awayBatter,omitempty"`
+
+	// American Football
+	Possession *int `xml:"possession,attr,omitempty" json:"possession,omitempty" bson:"possession,omitempty"`
+	Position   *int `xml:"position,attr,omitempty" json:"position,omitempty" bson:"position,omitempty"`
+	Try        *int `xml:"try,attr,omitempty" json:"try,omitempty" bson:"try,omitempty"`
+	Yards      *int `xml:"yards,attr,omitempty" json:"yards,omitempty" bson:"yards,omitempty"`
+
+	// Snooker
+	RemainingReds *int `xml:"remaining_reds,attr,omitempty" json:"remainingReds,omitempty" bson:"remainingReds,omitempty"`
+
+	// Darts and Snooker
+	Visit *int `xml:"visit,attr,omitempty" json:"visit,omitempty" bson:"visit,omitempty"`
+
+	// Darts
+	HomeLegscore *int `xml:"home_legscore,attr,omitempty" json:"homeLegscore,omitempty" bson:"homeLegscore,omitempty"`
+	AwayLegscore *int `xml:"away_legscore,attr,omitempty" json:"awayLegscore,omitempty" bson:"awayLegscore,omitempty"`
+	Throw        *int `xml:"throw,attr,omitempty" json:"throw,omitempty" bson:"throw,omitempty"`
+
+	// Bowls
+	Delivery           *int `xml:"delivery,attr,omitempty" json:"delivery,omitempty" bson:"delivery,omitempty"`
+	HomeRemainingBowls *int `xml:"home_remaining_bowls,attr,omitempty" json:"homeRemainingBowls,omitempty" bson:"homeRemainingBowls,omitempty"`
+	AwayRemainingBowls *int `xml:"away_remaining_bowls,attr,omitempty" json:"awayRemainingBowls,omitempty" bson:"awayRemainingBowls,omitempty"`
+	CurrentEnd         *int `xml:"current_end,attr,omitempty" json:"currentEnd,omitempty" bson:"currentEnd,omitempty"`
+
+	// Cricket
+	Innings         *int `xml:"innings,attr,omitempty" json:"innings,omitempty" bson:"innings,omitempty"`
+	Over            *int `xml:"over,attr,omitempty" json:"over,omitempty" bson:"over,omitempty"`
+	HomePenaltyRuns *int `xml:"home_penalty_runs,attr,omitempty" json:"homePenaltyRuns,omitempty" bson:"homePenaltyRuns,omitempty"`
+	AwayPenaltyRuns *int `xml:"away_penalty_runs,attr,omitempty" json:"awayPenaltyRuns,omitempty" bson:"awayPenaltyRuns,omitempty"`
+	HomeDismissals  *int `xml:"home_dismissals,attr,omitempty" json:"homeDismissals,omitempty" bson:"homeDismissals,omitempty"`
+	AwayDismissals  *int `xml:"away_dismissals,attr,omitempty" json:"awayDismissals,omitempty" bson:"awayDismissals,omitempty"`
+
+	// CS:GO
+	CurrentCtTeam *Team `xml:"current_ct_team,attr,omitempty" json:"currentCtTeam,omitempty" bson:"currentCtTeam,omitempty"`
 
 	Clock        *Clock        `xml:"clock,omitempty" json:"clock,omitempty" bson:"clock,omitempty"`
 	PeriodScores []PeriodScore `xml:"period_scores>period_score,omitempty" json:"periodScores,omitempty" bson:"periodScores,omitempty"`
@@ -139,6 +161,7 @@ type Result struct {
 type Statistics struct {
 	YellowCards    *StatisticsScore `xml:"yellow_cards,omitempty" json:"yellowCards,omitempty" bson:"yellowCards,omitempty"`
 	RedCards       *StatisticsScore `xml:"red_cards,omitempty" json:"redCards,omitempty" bson:"redCards,omitempty"`
+	GreenCards     *StatisticsScore `xml:"green_cards,omitempty" json:"greenCards,omitempty" bson:"greenCards,omitempty"`
 	YellowRedCards *StatisticsScore `xml:"yellow_red_cards,omitempty" json:"yellowRedCards,omitempty" bson:"yellowRedCards,omitempty"`
 	Corners        *StatisticsScore `xml:"corners,omitempty" json:"corners,omitempty" bson:"corners,omitempty"`
 }
